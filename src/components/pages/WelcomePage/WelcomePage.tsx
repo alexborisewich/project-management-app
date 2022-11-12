b9160595dec98023aded946c0cc0fa55e7dfcbe4
Igor Pavetka <uropb.86@gmail.com>
2022-11-12 22:54:20 +0300

    feat: add basic structure and content of Welcome-page

--- src/components/pages/WelcomePage/WelcomePage.tsx
+++ src/components/pages/WelcomePage/WelcomePage.tsx
@@ -4,7 +4,34 @@ import { s, types } from './';
 
 const WelcomePage = ({ dataTestId }: types.WelcomePageProps) => (
   <section className={s.container} data-testid={dataTestId}>
-    WelcomePage
+    <h1>Welcome to Project manager application</h1>
+    <p>We try to create the best app of todo list</p>
+    <div className={s.wrapper__creator}>
+      <div className={s.creator}>
+        <h3>Aliaksei Barysewich</h3>
+        <ul className={s.ul}>
+          <li>...</li>
+          <li>...</li>
+          <li>...</li>
+        </ul>
+      </div>
+      <div className={s.creator}>
+        <h3>Ihar Pavetka</h3>
+        <ul className={s.ul}>
+          <li>...</li>
+          <li>...</li>
+          <li>...</li>
+        </ul>
+      </div>
+      <div className={s.creator}>
+        <h3>Aliaksandr Sakalouski</h3>
+        <ul className={s.ul}>
+          <li>...</li>
+          <li>...</li>
+          <li>...</li>
+        </ul>
+      </div>
+    </div>
   </section>
 );
 
