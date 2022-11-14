4f4c20ee20801ad3c1610202914ca3cc050ef401
AlexGradus <countr@mail.ru>
2022-11-14 17:10:30 +0300

    feat:add Local Api and Board List

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { s, types } from './';
 
-const Board = ({ dataTestId }: types.BoardProps) => (
-  <div className={s.container} data-testid={dataTestId}>
-    Board
-  </div>
-);
+import { decode, getBoardsById } from './LocalApi';
+//import { createBoard, createUser, decode, getBoardsById, getUsers, signIn } from './LocalApi';
+//import { s, types } from './';
+//void createUser({
+// name: 'Mida q',
+// login: 'Molodai',
+// password: 'fudzam',
+//});
+//void signIn({
+//login: 'Molodai',
+//password: 'fudzam',
+//});
+//const solution = void getUsers().then((solve) => {
+//console.log(solve);
+//});
+
+//const createB = void createBoard({
+//// title: 'hiMaan3',
+// owner: decode() as string,
+//users: ['Che Tam'],
+//});
+//const boardById = void getBoardsById(decode() as string).then((solve) => {
+//  console.log(solve);
+//});
+
+const Board = function ({ dataTestId }: types.BoardProps) {
+  const [allBoardsById, setAllBoardsById] = useState([{ title: 'Hello!', users: ['First Description!'] }]);
+
+  useEffect(() => {
+    void getBoardsById(decode() as string).then((solve) => {
+      setAllBoardsById(solve ? solve : [{ title: 'Hello!', users: ['First Description!'] }]);
+    });
+  }, []);
+  return (
+    <div className={s.container} data-testid={dataTestId}>
+      {allBoardsById.map((item, index) => (
+        <div key={index}>
+          <div>
+            <p className={s.container}>{item.title}</p>
+            <p className={s.container}>{item.users}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Board;
---
+++ src/components/Board/LocalConstant.tsx
@@ -0,0 +1,1 @@
+export const BASEURL = 'https://final-task-backend-production-c2d6.up.railway.app';
