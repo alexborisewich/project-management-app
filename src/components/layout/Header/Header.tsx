1e4707725f6c9156d8c92e3ef9e3f9ae73bc8373
Igor Pavetka <uropb.86@gmail.com>
2022-11-14 22:34:51 +0300

    feat: add basic structure and content of Header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,7 +4,13 @@ import { s, types } from './';
 
 const Header = ({ dataTestId }: types.HeaderProps) => (
   <header className={s.container} data-testid={dataTestId}>
-    Header
+    <span>Logo</span>
+    <div className={s.wrapper__btns}>
+      <button className={s.btn}>Sign In</button>
+      <button className={s.btn}>Sign Up</button>
+      <button className={s.btn}>Color</button>
+      <button className={s.btn}>Eng/Rus</button>
+    </div>
   </header>
 );
 
