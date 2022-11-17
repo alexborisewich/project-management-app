8abd589b34bb02254903c663cb2c287e7bd0e8f4
Igor Pavetka <uropb.86@gmail.com>
2022-11-17 09:30:28 +0300

    refactor: rewrite Welcome-Page according style layout

--- src/components/pages/WelcomePage/Creator/Creator.tsx
+++ src/components/pages/WelcomePage/Creator/Creator.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { s, types } from '.';
 
+import Photo from './../../../../assets/images/Fronted.png';
+
 const Creator = ({ name, feature, dataTestId }: types.CreatorProps) => (
   <div className={s.creator} data-testid={dataTestId}>
-    <h3>{name}</h3>
+    <img src={Photo} alt='Creator' />
+    <h3 className={s.name}>{name}</h3>
     <ul className={s.ul}>
       {feature.map((el, index) => (
         <li key={index}>{el}</li>
--- src/components/pages/WelcomePage/WelcomePage.tsx
+++ src/components/pages/WelcomePage/WelcomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { s, types } from './';
 
+import Image from './../../../assets/images/main.png';
 import Creator from './Creator';
 const info = [
   {
@@ -20,12 +21,23 @@ const info = [
 
 const WelcomePage = ({ dataTestId }: types.WelcomePageProps) => (
   <section className={s.container} data-testid={dataTestId}>
-    <h1>Welcome to Project manager application</h1>
-    <p>We try to create the best app of todo list</p>
-    <div className={s.wrapper__creator}>
-      {info.map((el, index) => (
-        <Creator name={el.name} feature={el.feature} key={index} />
-      ))}
+    <div className={s.block__wrapper}>
+      <h1>Welcome to Project manager application</h1>
+      <p>
+        Kanban - is an application with a wide range of possibilities for creating and managing boards. React 2022Q1
+      </p>
+    </div>
+    <div className={s.block__wrapper}>
+      <img src={Image} alt='Welcome' className={s.img} />
+    </div>
+
+    <div className={s.block__wrapper_creator}>
+      <h2 className={s.creator__header}>About us</h2>
+      <div className={s.creator__div}>
+        {info.map((el, index) => (
+          <Creator name={el.name} feature={el.feature} key={index} />
+        ))}
+      </div>
     </div>
   </section>
 );
