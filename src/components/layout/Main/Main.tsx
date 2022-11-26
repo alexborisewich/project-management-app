0fe5fffe2c0bcfcfe2d8fefc064e1b2b81234532
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-11-26 23:52:01 +0300

    feat: add toast container

--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import { s, types } from './';
 
 const Main = ({ dataTestId }: types.MainProps) => (
   <main className={s.container} data-testid={dataTestId}>
     <Outlet />
+    <ToastContainer
+      position='bottom-right'
+      autoClose={2500}
+      hideProgressBar
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme='colored'
+    />
   </main>
 );
 
